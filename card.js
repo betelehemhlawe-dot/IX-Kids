1b36cbc3ad4962d88d397a89d45c3d9ed1fd7ecb
betelehemhlawe-dot <betelehemhlawe@gmail.com>
2025-10-03 11:15:23 +0300

    Add files via upload

---
+++ card.js
@@ -0,0 +1,6 @@
+export function Card({ children, className }) {
+  return <div className={`bg-white p-4 rounded-lg ${className}`}>{children}</div>;
+}
+export function CardContent({ children, className }) {
+  return <div className={className}>{children}</div>;
+}
