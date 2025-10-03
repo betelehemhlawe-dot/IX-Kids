e3491a5da9fc551d33f4db013a1c08af8f5aaeae
betelehemhlawe-dot <betelehemhlawe@gmail.com>
2025-10-03 11:15:04 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import KidsEduSite from './KidsEduSite';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<KidsEduSite />);
